7007474e37e34165efbe5e98a2c5c815a8700c08
varan <varan2030@mail.ru>
2020-02-04 22:20:21 -0500

    Updated config

--- src/config.js
+++ src/config.js
@@ -1,17 +1,17 @@
 export default {
 	s3: {
 		REGION: "us-east-1",
-		BUCKET: "fabishop-api-dev-attachmentsbucket-1kgblwfmh0cg3"
+		BUCKET: "fabishop-api-dev-attachmentsbucket-lvefip5casun"
 	},
 	apiGateway: {
 		REGION: "us-east-1",
-		URL: "https://7npby8dg45.execute-api.us-east-1.amazonaws.com/dev"
+		URL: "https://58cue09r9c.execute-api.us-east-1.amazonaws.com/dev"
 	},
 	cognito: {
 		REGION: "us-east-1",
-		USER_POOL_ID: "us-east-1_wVpYp59hy",
-		APP_CLIENT_ID: "eb56rhm0d1lke8dbeorfo04ud",
-		IDENTITY_POOL_ID: "us-east-1:00ccb5d5-ecc3-44b5-885d-ffeb63997ce4"
+		USER_POOL_ID: "us-east-1_nwk6JbCTb",
+		APP_CLIENT_ID: "11vbldns38c0n3signjb1ch466",
+		IDENTITY_POOL_ID: "us-east-1:8f133d32-fb1f-40c6-8370-ad1f84682752"
 	},
 	MAX_ATTACHMENT_SIZE: 5000000,
 	STRIPE_KEY: "pk_test_BjkFtBK6eHhwmEbJxLsAdZt000RqEEOefO"
